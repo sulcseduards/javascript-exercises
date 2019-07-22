0f0ea6a53603f0c1c05da0902c4de27596d8eb5f
Sandris Artemjevs <sandris.artemjevs@wandoofinance.com>
2019-07-22 20:11:22 +0300

    few more medium exercises

---
+++ exercises/medium/DiagonalAttack/index.js
@@ -0,0 +1,9 @@
+/**
+ * Diagonal Attack
+ * 
+ * Write a JS function that reads a given matrix of numbers, and checks if both main diagonals have equal sum. If they do, set every element that is NOT part of the main diagonals to that sum, alternatively just print the matrix unchanged.
+ *
+ * The input comes as array of strings. Each element represents a string of numbers, with spaces between them. Parse it into a matrix of numbers, so you can work with it.
+ *
+ * The output is either the new matrix, with all cells not belonging to a main diagonal changed to the diagonal sum or the original matrix, if the two diagonals have different sums. You need to print every row on a new line, with cells separated by a space.
+ */
